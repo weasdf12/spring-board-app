4f5c2e6dee90e41c3100db4312959ac1c9898052
weasdf12 <jjyyjj118@naver.com>
2025-09-26 13:57:09 +0900

    commit 0.0.1

---
+++ src/main/java/kr/co/sboard/config/AppConfig.java
@@ -0,0 +1,24 @@
+package kr.co.sboard.config;
+
+import kr.co.sboard.intercepter.AppInfoIntercepter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+
+    private AppInfo appInfo;
+
+    @Bean
+    public AppInfo getAppInfo() {
+        appInfo = new AppInfo();
+        return appInfo;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AppInfoIntercepter(appInfo));
+    }
+}
---
+++ src/main/java/kr/co/sboard/controller/UserController.java
@@ -0,0 +1,40 @@
+package kr.co.sboard.controller;
+
+import kr.co.sboard.config.AppInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class UserController {
+
+
+    private final AppInfo appInfo;
+
+    @GetMapping("/user/info")
+    public String info(){
+        return "user/info";
+    }
+
+    @GetMapping("/user/login")
+    public String login(Model model){
+        model.addAttribute(appInfo);
+        return "user/login";
+    }
+
+    @GetMapping("/user/register")
+    public String register(){
+        return "user/register";
+    }
+
+    @GetMapping("/user/terms")
+    public String terms(Model model){
+        model.addAttribute(appInfo);
+        return "user/terms";
+    }
+
+
+}
